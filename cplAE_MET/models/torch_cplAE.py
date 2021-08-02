0c01fc9df82a8d9c3358192e286af4ca7ebd6632
Fahimeh <fahimeh.baftizadeh@gmail.com>
2021-08-02 14:05:11 -0700

    MET model

--- cplAE_MET/models/torch_cplAE.py
+++ cplAE_MET/models/torch_cplAE.py
@@ -81,7 +81,7 @@ class Encoder_E(nn.Module):
     """
     Encoder for electrophysiology data
     TODO: Add gaussian noise: use this torch.normal(mean=torch.arange(1., 11.), std=torch.arange(1, 0, -0.1))
-    
+
     Args:
         per_feature_gaussian_noise_sd: std of gaussian noise injection if training=True
         in_dim: input size of data
@@ -95,14 +95,14 @@ class Encoder_E(nn.Module):
                  int_dim=40,
                  out_dim=3,
                  per_feature_gaussian_noise_sd=None,
-                 dropout_rate=0.1,
+                 dropout_p=0.1,
                  dtype=torch.FloatTensor):
 
 
         super(Encoder_E, self).__init__()
-        per_feature_gaussian_noise_sd = torch.as_tensor(per_feature_gaussian_noise_sd)
-        self.per_feature_gaussian_noise_sd = per_feature_gaussian_noise_sd.view(1,in_dim)
-        self.drp = nn.Dropout(p=dropout_rate)
+        # per_feature_gaussian_noise_sd = torch.as_tensor(per_feature_gaussian_noise_sd)
+        # self.per_feature_gaussian_noise_sd = per_feature_gaussian_noise_sd.view(1,in_dim)
+        self.drp = nn.Dropout(p=dropout_p)
         self.fc0 = nn.Linear(in_dim, int_dim)
         self.fc1 = nn.Linear(int_dim, int_dim)
         self.fc2 = nn.Linear(int_dim, int_dim)
@@ -120,12 +120,12 @@ class Encoder_E(nn.Module):
             batchsize = x.shape[0]
             x = torch.tensor(self.per_feature)
             x.repeat(batchsize, 0)
-            n = torch.normal(mean=torch.zeros(x.shape), std=)
+            n = torch.normal(mean=torch.zeros(x.shape), std=1)
             x = x + n
         return x
 
     def forward(self, x):
-        x = self.gnoise(self,x)
+        # x = self.gnoise(self,x)
         x = self.drp(x)
         x = self.elu(self.fc0(x))
         x = self.relu(self.fc1(x))
@@ -150,15 +150,19 @@ class Decoder_E(nn.Module):
                  in_dim=3,
                  int_dim=40,
                  out_dim=300,
-                 dropout_rate=0.1):
+                 dropout_p=0.1):
 
+        super(Decoder_E, self).__init__()
         self.fc0 = nn.Linear(in_dim, int_dim)
         self.fc1 = nn.Linear(int_dim, int_dim)
         self.fc2 = nn.Linear(int_dim, int_dim)
         self.fc3 = nn.Linear(int_dim, int_dim)
-        self.drp = nn.Dropout(p=dropout_rate)
+        self.drp = nn.Dropout(p=dropout_p)
         self.Xout = nn.Linear(int_dim, out_dim)
+        self.elu = nn.ELU()
+        self.relu = nn.ReLU()
         return
+
 
     def forward(self, x):
         x = self.elu(self.fc0(x))
@@ -176,12 +180,11 @@ class Encoder_M(nn.Module):
 
     Args:
         out_dim: representation dimenionality
+        std_dev: gaussian noise std dev
     """
 
-    def __init__(self,
-                std_dev=0.1,
-                out_dim=3,
-                **kwargs):
+    def __init__(self, std_dev=0.1,  out_dim=3):
+
         super(Encoder_M, self).__init__()
         self.gaussian_noise_std_dev=std_dev
         self.conv1_ax = nn.Conv2d(1, 10, kernel_size=(4, 3), stride=(4, 1), padding='valid')
@@ -228,9 +231,7 @@ class Decoder_M(nn.Module):
         in_dim: representation dimensionality
     """
 
-    def __init__(self,
-                 in_dim=3,
-                 **kwargs):
+    def __init__(self, in_dim=3):
 
         super(Decoder_M, self).__init__()
         self.fc1_dec = nn.Linear(in_dim, 20)
@@ -260,7 +261,7 @@ class Decoder_M(nn.Module):
 
         ax = self.convT2_ax(ax)
         de = self.convT2_de(de)
-        
+
         x = torch.cat(tensors=(ax, de), dim=1)
         return x
 
@@ -276,7 +277,7 @@ class Model_TE(nn.Module):
         T_dropout: dropout for T data
         E_dropout: dropout for E data
         latent_dim: dim for representations
-        alpha_T: loss weight for T reconstruction 
+        alpha_T: loss weight for T reconstruction
         alpha_E: loss weight for E reconstruction
         lambda_TE: loss weight coupling loss
         augment_decoders (bool): augment decoder with cross modal representation if True
@@ -347,3 +348,106 @@ class Model_TE(nn.Module):
 
         self.loss = sum(self.loss_dict.values())
         return zT, zE, XrT, XrE
+
+
+class Model_MET(nn.Module):
+    """Coupled autoencoder model with 3 agents for morphology(M), electrophysiology(E) and Transcriptomics(T)
+
+    Args:
+        T_dim: Number of genes in T data
+        E_dim: Number of genes in E data
+        T_int_dim: hidden layer dims for T model
+        E_int_dim: hidden layer dims for E model
+        T_dropout: dropout for T data
+        E_dropout: dropout for E data
+        latent_dim: dim for representations
+        alpha_T: loss weight for T reconstruction
+        alpha_E: loss weight for E reconstruction
+        alpha_M: loss weight for M reconstruction
+        lambda_TE: loss weight coupling loss between T and E
+        lambda_MT: loss weight coupling loss between M and T
+        lambda_ME: loss weight coupling loss between M and E
+        std_dev: gaussian noise std dev
+        augment_decoders (bool): augment decoder with cross modal representation if True
+    """
+
+    def __init__(self,
+                 T_dim=1000, T_int_dim=50, T_dropout=0.5,
+                 E_dim=1000, E_int_dim=50, E_dropout=0.5,
+                 latent_dim=3, alpha_T=1.0, alpha_E=1.0,
+                 alpha_M=1.0, lambda_TE=1.0, lambda_MT=1.0,
+                 lambda_ME=1.0, std_dev=1.0, augment_decoders=True):
+
+        super(Model_MET, self).__init__()
+        self.alpha_T = alpha_T
+        self.alpha_E = alpha_E
+        self.alpha_M = alpha_M
+        self.lambda_TE = lambda_TE
+        self.lambda_ME = lambda_ME
+        self.lambda_MT = lambda_MT
+        self.augment_decoders = augment_decoders
+
+        self.eT = Encoder_T(dropout_p=T_dropout, in_dim=T_dim, out_dim=latent_dim, int_dim=T_int_dim)
+        self.eE = Encoder_E(dropout_p=E_dropout, in_dim=E_dim, out_dim=latent_dim, int_dim=E_int_dim)
+        self.eM = Encoder_M(std_dev=std_dev, out_dim=latent_dim)
+        self.dT = Decoder_T(in_dim=latent_dim, out_dim=T_dim, int_dim=T_int_dim)
+        self.dE = Decoder_E(in_dim=latent_dim, out_dim=E_dim, int_dim=E_int_dim)
+        self.dM = Decoder_M(in_dim=latent_dim)
+        return
+
+    @staticmethod
+    def min_var_loss(zi, zj):
+        #SVD calculated over all entries in the batch
+        batch_size = zj.shape[0]
+        zj_centered = zj - torch.mean(zj, 0, True)
+        min_eig = torch.min(torch.linalg.svdvals(zj_centered))
+        min_var_zj = torch.square(min_eig)/(batch_size-1)
+
+        zi_centered = zi - torch.mean(zi, 0, True)
+        min_eig = torch.min(torch.linalg.svdvals(zi_centered))
+        min_var_zi = torch.square(min_eig)/(batch_size-1)
+
+        #Wij_paired is the weight of matched pairs
+        zi_zj_mse = torch.mean(torch.sum(torch.square(zi-zj), 1))
+        loss_ij = zi_zj_mse/torch.squeeze(torch.minimum(min_var_zi, min_var_zj))
+        return loss_ij
+
+    @staticmethod
+    def mean_sq_diff(x, y):
+        return torch.mean(torch.square(x-y))
+
+    def forward(self, inputs):
+        #T arm forward pass
+        XT = inputs[0]
+        zT = self.eT(XT)
+        XrT = self.dT(zT)
+
+        #E arm forward pass
+        XE = inputs[1]
+        zE = self.eE(XE)
+        XrE = self.dE(zE)
+
+        # M arm forward pass
+        XM = inputs[2]
+        zM = self.eM(XM)
+        XrM = self.dM(zM)
+
+        #Loss calculations
+        self.loss_dict = {}
+        self.loss_dict['recon_T'] = self.alpha_T * self.mean_sq_diff(XT, XrT)
+        self.loss_dict['recon_E'] = self.alpha_E * self.mean_sq_diff(XE, XrE)
+        self.loss_dict['recon_M'] = self.alpha_M * self.mean_sq_diff(XM, XrM)
+        self.loss_dict['cpl_TE'] = self.lambda_TE * self.min_var_loss(zT, zE)
+        self.loss_dict['cpl_ME'] = self.lambda_ME * self.min_var_loss(zM, zE)
+        self.loss_dict['cpl_MT'] = self.lambda_MT * self.min_var_loss(zM, zT)
+
+        if self.augment_decoders:
+            XrT_aug = self.dT(zE.detach())
+            XrE_aug = self.dE(zT.detach())
+            XrM_aug = self.dM(zM.detach()) #Do we have aug for M?
+            self.loss_dict['recon_T_aug'] = self.alpha_T * self.mean_sq_diff(XT, XrT_aug)
+            self.loss_dict['recon_E_aug'] = self.alpha_E * self.mean_sq_diff(XE, XrE_aug)
+            self.loss_dict['recon_M_aug'] = self.alpha_M * self.mean_sq_diff(XM, XrM_aug)
+
+        self.loss = sum(self.loss_dict.values())
+        return  zT, zE, zM, XrT, XrE, XrM
--- cplAE_MET/models/torch_train.py
+++ cplAE_MET/models/torch_train.py
@@ -5,69 +5,89 @@ from pathlib import Path
 import numpy as np
 import scipy.io as sio
 import torch
-from cplAE_MET.models.torch_cplAE import Model_TE
-from cplAE_MET.utils.proc import get_splits, select_dataset_v1
+from cplAE_MET.models.torch_cplAE import Model_MET
+# from cplAE_MET.utils.proc import get_splits, select_dataset_v1
 from torch.utils.data import DataLoader
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batchsize",         default=500,             type=int,    help="Batch size")
+parser.add_argument("--batchsize",         default=100,             type=int,    help="Batch size")
 parser.add_argument("--alpha_T",           default=1.0,             type=float,  help="Transcriptomic reconstruction loss weight")
-parser.add_argument("--alpha_E",           default=1.0,             type=float,  help="Epigenetic reconstruction loss weight")
-parser.add_argument("--lambda_TE" ,        default=1.0,             type=float,  help="Coupling loss weight")
+parser.add_argument("--alpha_E",           default=1.0,             type=float,  help="Electrophisiology reconstruction loss weight")
+parser.add_argument("--alpha_M",           default=1.0,             type=float,  help="Morphology reconstruction loss weight")
+parser.add_argument("--lambda_TE",         default=1.0,             type=float,  help="T and E coupling loss weight")
+parser.add_argument("--lambda_ME",         default=1.0,             type=float,  help="M and E coupling loss weight")
+parser.add_argument("--lambda_MT",         default=1.0,             type=float,  help="M and T coupling loss weight")
 parser.add_argument("--augment_decoders",  default=1,               type=int,    help="0 or 1 : Train with cross modal reconstruction")
 parser.add_argument("--latent_dim",        default=3,               type=int,    help="Number of latent dims")
-parser.add_argument("--n_epochs",          default=5000,            type=int,    help="Number of epochs to train")
+parser.add_argument("--n_epochs",          default=1000,            type=int,    help="Number of epochs to train")
 parser.add_argument("--run_iter",          default=0,               type=int,    help="Run-specific id")
-parser.add_argument("--model_id",          default='TE',            type=str,    help="Model-specific id")
-parser.add_argument("--exp_name",          default='snmCAT_torch',  type=str,    help="Experiment set")
+parser.add_argument("--model_id",          default='MET',           type=str,    help="Model-specific id")
+parser.add_argument("--exp_name",          default='MET_torch',     type=str,    help="Experiment set")
+
+#TODO
+
+# def set_paths(exp_name='TEMP'):
+#     # paths = load_config(verbose=False)
+#     paths = {}
+#     paths['result'] = f'{str(paths["package_dir"] / "data/results")}/{exp_name}/'
+#     paths['logs'] = f'{paths["result"]}logs/'
+#     Path(paths['logs']).mkdir(parents=True, exist_ok=True)
+#     return paths
+
+# def worker_init_fn(worker_id):
+#     #used only if using multiprocessing to load data.
+#     #see related discussion on pytorch github.
+#     np.random.seed(np.random.get_state()[1][0] + worker_id)
 
 
-def set_paths(exp_name='TEMP'):
-    paths = load_config(verbose=False)
-    paths['result'] = f'{str(paths["package_dir"] / "data/results")}/{exp_name}/'
-    paths['logs'] = f'{paths["result"]}logs/'
-    Path(paths['logs']).mkdir(parents=True, exist_ok=True)
-    return paths
-
-
-def worker_init_fn(worker_id):
-    #used only if using multiprocessing to load data. 
-    #see related discussion on pytorch github.
-    np.random.seed(np.random.get_state()[1][0] + worker_id)
-
-class TE_Dataset(torch.utils.data.Dataset):
-    def __init__(self, T_dat, E_dat):
-        super(TE_Dataset).__init__()
+class MET_Dataset(torch.utils.data.Dataset):
+    def __init__(self, T_dat, E_dat, M_dat):
+        super(MET_Dataset).__init__()
         self.T_dat = T_dat
         self.E_dat = E_dat
+        self.M_dat = M_dat
+        self.n_samples = np.shape(self.T_dat)[0]
 
     def __getitem__(self, idx):
-        return {'XT': self.T_dat[idx, :], 'XE': self.E_dat[idx, :]}
+        sample = {"XT": self.T_dat[idx, :],
+                  "XE": self.E_dat[idx, :],
+                  "XM": self.M_dat[idx, :]}
+        return sample
 
     def __len__(self):
-        return np.shape(self.T_dat)[0]
+        return self.n_samples
 
 
-def main(alpha_T=1.0, alpha_E=1.0, lambda_TE=1.0, augment_decoders=1.0,
-         batchsize=500, latent_dim=3, n_epochs=5000,
-         run_iter=0, model_id='TE', exp_name='snmCAT_torch'):
+def main(alpha_T=1.0, alpha_E=1.0, alpha_M=1.0, lambda_TE=1.0, lambda_ME=1.0,
+         lambda_MT=1.0, augment_decoders=1.0, batchsize=500, latent_dim=3,
+         n_epochs=5000, run_iter=0, model_id='MET', exp_name='MET_torch'):
 
-    dir_pth = set_paths(exp_name=exp_name)
+    #TODO
+
+    # dir_pth = set_paths(exp_name=exp_name)
+    dir_pth = {}
+    dir_pth['result'] = "/Users/fahimehb/Documents/git-workspace/cplAE_MET/data/results"
+    dir_pth['logs'] = "/Users/fahimehb/Documents/git-workspace/cplAE_MET/data/logs"
+
     fileid = (model_id +
-              f'_aT_{str(alpha_T)}_aE_{str(alpha_E)}_' +
-              f'csTE_{str(lambda_TE)}_ad_{str(augment_decoders)}_' +
-              f'ld_{latent_dim:d}_bs_{batchsize:d}_ne_{n_epochs:d}_' +
+              f'_aT_{str(alpha_T)}_aE_{str(alpha_E)}_aM_{str(alpha_M)}_' +
+              f'csTE_{str(lambda_TE)}_csME_{str(lambda_ME)}_csMT_{str(lambda_MT)}_' +
+              f'ad_{str(augment_decoders)}_ld_{latent_dim:d}_bs_{batchsize:d}_ne_{n_epochs:d}_' +
               f'ri_{run_iter:d}').replace('.', '-')
 
     #Convert int to boolean
     augment_decoders = augment_decoders > 0
 
     #Data selection===================
-    n_genes = 1000
-    D = select_dataset_v1(n_genes,
-                          select_T='sorted_highvar_T_genes',
-                          select_E='sorted_highvar_E_genes')
-    train_ind, val_ind = get_splits(data=D, fold=0, n_folds=10)
+    n_genes = 1250
+    n_E_features = 120
+    D={}
+    D["XM"] = np.random.randn(1000, 2, 120, 4)
+    D["XT"] = np.random.randn(1000, 1250)
+    D["XE"] = np.random.randn(1000, 120)
+    train_ind = [i for i in range(0, 800)]
+    val_ind = [i for i in range(800, 1000)]
+    #train_ind, val_ind = get_splits(data=D, fold=0, n_folds=10) #Is it different than sklearn split? what is fold=0
     splits = {'train_ind': train_ind, 'val_ind': val_ind}
 
     #Helpers ==========================
@@ -96,24 +116,31 @@ def main(alpha_T=1.0, alpha_E=1.0, lambda_TE=1.0, augment_decoders=1.0,
 
     def save_results(model, data, fname, splits=splits):
         model.eval()
-        zT, zE, XrT, XrE = model((tensor_(data['XT']), tensor_(data['XE'])))
+        zT, zE, zM, XrT, XrE, XrM = model(
+            (tensor_(data['XT']),
+             tensor_(data['XE']),
+             (tensor_(data['XM']))))
         savemat = {'zT': tonumpy(zT), 'XrT': tonumpy(XrT), 
-                   'zE': tonumpy(zE), 'XrE': tonumpy(XrE)}
+                   'zE': tonumpy(zE), 'XrE': tonumpy(XrE),
+                   'zM': tonumpy(zM), 'XrM': tonumpy(XrM)}
         savemat.update(splits)
         sio.savemat(fname, savemat, do_compression=True)
         return
 
     #Training data
-    train_dataset = TE_Dataset(T_dat=D['XT'][train_ind, :], E_dat=D['XE'][train_ind, :])
+    train_dataset = MET_Dataset(T_dat=D['XT'][train_ind, :],
+                                E_dat=D['XE'][train_ind, :],
+                                M_dat=D['XM'][train_ind, :])
     train_sampler = torch.utils.data.RandomSampler(train_dataset, replacement=False)
     train_dataloader = DataLoader(train_dataset, batch_size=batchsize, shuffle=False,
                                   sampler=train_sampler, drop_last=True, pin_memory=True)
 
     #Model ============================
-    model = Model_TE(T_dim=n_genes, T_int_dim=50, T_dropout=0.2,
-                     E_dim=n_genes, E_int_dim=50, E_dropout=0.2,
-                     latent_dim=latent_dim, alpha_T=alpha_T, alpha_E=alpha_E, lambda_TE=lambda_TE, 
-                     augment_decoders=augment_decoders)
+    model = Model_MET(T_dim=n_genes, T_int_dim=50, T_dropout=0.2,
+                      E_dim=n_E_features, E_int_dim=50, E_dropout=0.2,
+                      latent_dim=latent_dim, alpha_T=alpha_T, alpha_E=alpha_E, alpha_M=alpha_M,
+                      lambda_TE=lambda_TE, lambda_ME=lambda_ME, lambda_MT=lambda_MT,
+                      augment_decoders=augment_decoders)
     optimizer = torch.optim.Adam(model.parameters())
 
     model.to(device)
@@ -129,7 +156,9 @@ def main(alpha_T=1.0, alpha_E=1.0, lambda_TE=1.0, augment_decoders=1.0,
             
             #zero + forward + backward + udpate
             optimizer.zero_grad()
-            _ = model((tensor_(batch['XT']),tensor_(batch['XE'])))
+            _ = model((tensor_(batch['XT']),
+                       tensor_(batch['XE']),
+                       tensor_(batch['XM'])))
             model.loss.backward()
             optimizer.step()
             
@@ -139,7 +168,7 @@ def main(alpha_T=1.0, alpha_E=1.0, lambda_TE=1.0, augment_decoders=1.0,
         #Validation: train mode -> eval mode + no_grad + eval mode -> train mode
         model.eval()
         with torch.no_grad():
-            _ = model((tensor_(D['XT'][val_ind, :]),tensor_(D['XE'][val_ind, :])))
+            _ = model((tensor_(D['XT'][val_ind, :]), tensor_(D['XE'][val_ind, :]), tensor_(D['XM'][val_ind, :])))
 
         val_loss_dict = collect_losses(model_loss=model.loss_dict, tracked_loss={})
         model.train()
