788537c1308bab45ea8dcb038da52d4bff8aa8e6
Ian Convy <ian.convy@alleninstitute.org>
2024-02-15 14:01:55 -0800

    Added support for TorchScript models.

--- utils.py
+++ utils.py
@@ -75,6 +75,15 @@ def load_model(config, state_dict):
     model.load_state_dict(state_dict)
     return model
 
+def assemble_jit(jit_path, device = "cpu"):
+    model = {}
+    modalities = [path.stem for path in (jit_path / "encoder").iterdir()]
+    for modal in modalities:
+        model[modal] = {}
+        model[modal]["enc"] = torch.jit.load(jit_path / "encoder" / f"{modal}.pt", map_location = device)
+        model[modal]["dec"] = torch.jit.load(jit_path / "decoder" / f"{modal}.pt", map_location = device)
+    return model
+
 def load_cross_validation(exp_dir, get_checkpoints = False):
     exp_path = project_dir / "data" / exp_dir
     results = {}
@@ -96,6 +105,34 @@ def load_cross_validation(exp_dir, get_checkpoints = False):
                 state = torch.load(path, torch.device("cpu"))
                 info_dict["checkpoints"][epoch] = state
         results["folds"].append(info_dict)
+    return results
+
+def load_jit_folds(exp_path, folds = None, get_checkpoints = False):
+    exp_path = pathlib.Path(exp_path)
+    results = {}
+    with open(exp_path / "config.yaml", "r") as target:
+        results["config"] = yaml.safe_load(target)
+    fold_paths = list(exp_path.glob("fold_*"))
+    fold_paths.sort(key = lambda path: int(path.stem.split("_")[-1]))
+    if folds is not None:
+        fold_paths = [path for path in fold_paths if int(path.name.split("_")[-1]) in folds]
+    results["folds"] = {}
+    for fold_path in fold_paths:
+        fold = int(fold_path.stem.split("_")[-1])
+        info_dict = {}
+        specimen_ids = np.load(fold_path / "train_test_ids.npz")
+        info_dict["train_ids"] = specimen_ids["train"]
+        info_dict["test_ids"] = specimen_ids["test"]
+        info_dict["best"] = assemble_jit(fold_path / "best")
+        if get_checkpoints:
+            info_dict["checkpoints"] = {}
+            paths = list((fold_path / "checkpoints").iterdir())
+            paths.sort(key = lambda path: int(path.stem.split("_")[-1]))
+            for path in paths:
+                epoch = int(path.stem.split("_")[-1])
+                state = assemble_jit(path)
+                info_dict["checkpoints"][epoch] = state
+        results["folds"][fold] = info_dict
     return results
 
 def load_pca_cca(exp_dir):
